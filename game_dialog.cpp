fcbffa4cf1eacceacbd9ff0a83398530ac677922
richelbilderbeek <richel@richelbilderbeek.nl>
2019-03-02 12:42:42 +0100

    Done

--- game_dialog.cpp
+++ game_dialog.cpp
@@ -6,9 +6,31 @@ game_dialog::game_dialog(QWidget *parent) :
   ui(new Ui::game_dialog)
 {
   ui->setupUi(this);
+  this->startTimer(1000);
 }
 
 game_dialog::~game_dialog()
 {
   delete ui;
+}
+
+void game_dialog::mousePressEvent(QMouseEvent *event)
+{
+  m_beer_state = beer_state::foam;
+  ui->label_beer->setPixmap(QPixmap(":/beer_with_foam.png"));
+  ui->label_text->setText("Yummie!\nBeer with foam!");
+}
+
+void game_dialog::timerEvent(QTimerEvent *event)
+{
+  if (m_beer_state == beer_state::foam)
+  {
+    if (std::rand() % 10 == 0)
+    {
+      m_beer_state = beer_state::no_foam;
+      ui->label_beer->setPixmap(QPixmap(":/beer_no_foam.png"));
+      ui->label_text->setText("Yuck! Beer\nwithout foam!");
+    }
+  }
+
 }
--- game_dialog.h
+++ game_dialog.h
@@ -12,11 +12,20 @@ class game_dialog : public QDialog
   Q_OBJECT
 
 public:
+
+  enum class beer_state { foam, no_foam };
+
   explicit game_dialog(QWidget *parent = 0);
   ~game_dialog();
 
 private:
   Ui::game_dialog *ui;
+
+  beer_state m_beer_state = beer_state::foam;
+
+  void mousePressEvent(QMouseEvent *event);
+
+  void timerEvent(QTimerEvent *event);
 };
 
 #endif // GAME_DIALOG_H
